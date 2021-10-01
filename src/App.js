88e6493e8bec972e19c0e7266f8c74e4f38d7293
nguye374 <nguye374@sheridancollege.ca>
2021-10-01 12:04:46 -0400

    Oct 1 third

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import ProductDetail from './pages/ProductDetail';
@@ -9,15 +9,20 @@ function App() {
     <div>
       <MainHeader/>
       <main>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/welcome"/>
+        </Route>
         <Route path="/welcome">
           <Welcome/>
         </Route>
-        <Route path="/products">
+        <Route path="/products" exact>
           <Products/>
         </Route>
-        <Route path="/product-detail">
+        <Route path="/products/:productId">
           <ProductDetail/>
         </Route>
+      </Switch>
       </main>
     </div>
   );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const Products = () => {
     return (
         <section>
             <h1>Products Page</h1>
             <ul>
-                <li>Product 1</li>
-                <li>Product 2</li>
-                <li>Product 3</li>
+                <li><Link to="/products/book">Book</Link></li>
+                <li><Link to="/products/car">Car</Link></li>
+                <li><Link to="/products/course">Course</Link></li>
             </ul>
         </section>
     )
